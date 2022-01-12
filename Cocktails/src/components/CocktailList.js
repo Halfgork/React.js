625472ff510b36da44079d67968afd5ec0a4290b
Görkem Şakalak <59141691+Halfgork@users.noreply.github.com>
2022-01-12 03:25:58 +0300

    Fix

--- Cocktails/src/components/CocktailList.js
+++ Cocktails/src/components/CocktailList.js
@@ -11,7 +11,7 @@ export default function CocktailList() {
   if (cocktails.length < 1) {
     return (
       <h2 className='section-title'>
-        no cocktails matched your search criteria
+        Nothing matched your search criteria
       </h2>
     )
   }
